3234d2c12ff08fda9d1931331d4e6a3f1c6564f4
Dabsia <dabojohnson@gmail.com>
2022-05-04 15:26:07 -0700

    Made the collection overview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,15 @@
-import React , {Component}  from 'react';
-import  shop_Data from './shopData';
-import CollectionPreview from  '../../components/collection-preview/collection-preview.component'
-class ShopPage extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            collections : shop_Data
-        }
-    }
-    render() {
-        const {collections} = this.state;
+import React from 'react';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+
+const ShopPage = ({collections}) =>{
+    
         return (
             <div className='shop-page'>
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key = {id} {...otherCollectionProps}/>
-                    ))
-                }
+                <CollectionsOverview />
             </div>
         )
-    }
+    
 }
+
 
 export default ShopPage
